9a7c9d3a9f21faa85aa59dde175e43cd83443e5e
tamta123 <tamtag186@gmail.com>
2023-12-28 16:08:43 +0400

    wip:make code correction to find the error origine

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -89,7 +89,7 @@ export const login = async (req, res) => {
     }
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ message: "ტამტა" });
   }
 };
 
