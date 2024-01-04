b3a7dde08c7809179ffeffba8f3e72bca01248a5
tamta123 <tamtag186@gmail.com>
2024-01-04 11:32:18 +0400

    wip:catch the error

--- src/mail/index.js
+++ src/mail/index.js
@@ -11,10 +11,10 @@ const gmailTransport = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    // user: process.env.GMAIL_USER,
-    // pass: process.env.GMAIL_PASSWORD,
-    user: "tamtag186@gmail.com",
-    pass: "thke ofvh qsjp lzai",
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD,
+    // user: "tamtag186@gmail.com",
+    // pass: "thke ofvh qsjp lzai",
   },
   debug: true, // Enable debugging
 });
