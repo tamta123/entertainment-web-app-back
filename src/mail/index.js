cf851e1f957ffc02c95623500f4792620d8898f3
tamta123 <tamtag186@gmail.com>
2024-01-10 17:06:06 +0400

    wip:find the varifiacition error

--- src/mail/index.js
+++ src/mail/index.js
@@ -2,12 +2,13 @@ import dotenv from "dotenv";
 import nodemailer from "nodemailer";
 
 dotenv.config();
-console.log("name", process.env.GMAIL_USER);
+console.log("name", process.env.GMAIL_USER.trim());
 console.log("password", process.env.GMAIL_PASSWORD);
+console.log("All environment variables:", process.env);
 
 const gmailTransport = nodemailer.createTransport({
   // ტრანსპორტერის ფუნქციას ვქმნიტ რომელიც დააკავშირებს ნოუდ მეილერს(ჩვენს მეილს) ჯიმელითან,
-  host: "smtp.gmail.com",
+  service: "smtp.gmail.com",
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASSWORD,
