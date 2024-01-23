aab5c5aeb5569165cb19307a16f3aba9a7f9704b
tamta123 <tamtag186@gmail.com>
2024-01-23 16:16:03 +0400

    wip:update the user-router file

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -4,11 +4,11 @@ import {
   getAllUsers,
   verifyEmail,
 } from "../controllers/users-controller.js";
-import userMiddleware from "../middlewares/user-middleware.js";
+import saveUser from "../middlewares/user-middleware.js";
 
 const usersRouter = express.Router();
-usersRouter.get("/users", userMiddleware, getAllUsers);
-usersRouter.post("/register", userMiddleware.saveUser, addUser);
+usersRouter.get("/users", getAllUsers);
+usersRouter.post("/register", saveUser, addUser);
 usersRouter.get("/verify-email/:id/:token", verifyEmail);
 
 // usersRouter.post("/login", userMiddleware, login);
