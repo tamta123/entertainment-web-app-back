ca3fdde7c8b71f397b8b3d5b8300aa07dcc20f38
tamta123 <tamtag186@gmail.com>
2024-01-23 10:02:13 +0400

    wip: update the middleware  and mail files

--- src/mail/index.js
+++ src/mail/index.js
@@ -1,47 +1,77 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import nodemailer from "nodemailer";
 
-dotenv.config();
-console.log("name", process.env.GMAIL_USER.trim());
-console.log("password", process.env.GMAIL_PASSWORD);
-// console.log("All environment variables:", process.env);
+//function to send email to the user
+module.exports.sendingMail = async ({ from, to, subject, text }) => {
+  try {
+    let mailOptions = {
+      from,
+      to,
+      subject,
+      text,
+    };
+    //assign createTransport method in nodemailer to a variable
+    //service: to determine which email platform to use
+    //auth contains the senders email and password which are all saved in the .env
 
-const gmailTransport = nodemailer.createTransport({
-  // ტრანსპორტერის ფუნქციას ვქმნიტ რომელიც დააკავშირებს ნოუდ მეილერს(ჩვენს მეილს) ჯიმელითან,
-  service: "gmail",
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASSWORD,
-  },
-  debug: true,
-});
+    const Transporter = nodemailer.createTransport({
+      // ტრანსპორტერის ფუნქციას ვქმნიტ რომელიც დააკავშირებს ნოუდ მეილერს(ჩვენს მეილს) ჯიმელითან,
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+      debug: true,
+    });
+    //return the Transporter variable which has the sendMail method to send the mail
+    //which is within the mailOptions
+    return await Transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export default gmailTransport;
+// dotenv.config();
+// console.log("name", process.env.GMAIL_USER.trim());
+// console.log("password", process.env.GMAIL_PASSWORD);
+// // console.log("All environment variables:", process.env);
 
-//copilot
+// const gmailTransport = nodemailer.createTransport({
+//   // ტრანსპორტერის ფუნქციას ვქმნიტ რომელიც დააკავშირებს ნოუდ მეილერს(ჩვენს მეილს) ჯიმელითან,
+//   service: "gmail",
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GMAIL_PASSWORD,
+//   },
+//   debug: true,
+// });
 
-// import nodemailer from "nodemailer"
-// import { codeConfirmationTemplate } from "mail"
+// export default gmailTransport;
 
-// export const sendCodeConfirmation = async () => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.MAIL_USER,
-//       pass: process.env.MAIL_PASSWORD,
-//     },
-//   })
-//   const mailOptions = {
-//     from: <${process.env.EMAIL_FROM}>,
-//     to: email,
-//     subject: "MLTR Verification Code",
-//     html: codeConfirmationTemplate(code),
-//   }
-//   transporter.sendMail(mailOptions, (err: any, info: any) => {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       console.log(info)
-//     }
-//   })
-// }
+// //copilot
+
+// // import nodemailer from "nodemailer"
+// // import { codeConfirmationTemplate } from "mail"
+
+// // export const sendCodeConfirmation = async () => {
+// //   const transporter = nodemailer.createTransport({
+// //     service: "gmail",
+// //     auth: {
+// //       user: process.env.MAIL_USER,
+// //       pass: process.env.MAIL_PASSWORD,
+// //     },
+// //   })
+// //   const mailOptions = {
+// //     from: <${process.env.EMAIL_FROM}>,
+// //     to: email,
+// //     subject: "MLTR Verification Code",
+// //     html: codeConfirmationTemplate(code),
+// //   }
+// //   transporter.sendMail(mailOptions, (err: any, info: any) => {
+// //     if (err) {
+// //       console.log(err)
+// //     } else {
+// //       console.log(info)
+// //     }
+// //   })
+// // }
--- src/middlewares/user-middleware.js
+++ src/middlewares/user-middleware.js
@@ -1,24 +1,57 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 
-const userMiddleware = (request, response, next) => {
-  const { authorization } = request.headers;
-  if (!authorization) {
-    response.status(403).send();
-  } else {
-    const [, token] = authorization.trim().split(" ");
+// Function to check if username or email already exists in the database
+// This is to avoid having two users with the same username and email
 
-    try {
-      const verified = jwt.verify(token, process.env.JWT_SECRET);
-      if (verified) {
-        next();
-      } else {
-        response.status(403).send();
-      }
-    } catch (error) {
-      response.status(403).send();
+const saveUser = async (req, res, next) => {
+  try {
+    // Search the database to see if the username already exists
+    const userNameExist = await User.findOne({
+      where: {
+        firstName: req.body.userName,
+      },
+    });
+    // If username exists in the database, respond with a status of 409
+    if (userNameExist) {
+      return res.status(409).send("Username already taken");
     }
+    // Checking if the email already exists
+    const emailExist = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    // If email exists in the database, respond with a status of 409
+    if (emailExist) {
+      return res.status(409).send("email already taken");
+    }
+    // Continue to the next middleware or route handler if everything is okay
+    next();
+  } catch (error) {
+    console.error(error);
+    // Handle other errors or respond with an appropriate status code
+    res.status(500).send("Internal Server Error");
   }
 };
 
-export default userMiddleware;
+export default saveUser;
+
+// const userMiddleware = (request, response, next) => {
+//   const { authorization } = request.headers;
+//   if (!authorization) {
+//     response.status(403).send();
+//   } else {
+//     const [, token] = authorization.trim().split(" ");
+
+//     try {
+//       const verified = jwt.verify(token, process.env.JWT_SECRET);
+//       if (verified) {
+//         next();
+//       } else {
+//         response.status(403).send();
+//       }
+//     } catch (error) {
+//       response.status(403).send();
+//     }
+//   }
+// };
