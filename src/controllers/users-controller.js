d6b30e417ff07b2adfd8a99ac8ff86666e2a46a7
tamta123 <tamtag186@gmail.com>
2024-07-13 12:32:41 +0400

    wip:return token

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -73,7 +73,7 @@ export const addUser = async (req, res) => {
       }
 
       //send user details
-      return res.status(201).send({ user });
+      return res.status(201).json({ ...user.toJSON(), token });
     }
   } catch (error) {
     // Handle the error here
@@ -174,7 +174,7 @@ export const login = async (req, res) => {
           console.log("user", JSON.stringify(user, null, 2));
           console.log(token);
           //send user data
-          return res.status(201).send(user, token);
+          return res.status(200).json({ ...user.toJSON(), token });
         } else {
           return res.status(401).send("user not verified");
         }
