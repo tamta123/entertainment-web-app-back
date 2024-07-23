4b275d1684aa9a8690b2260ada700795ba7aa639
tamta123 <tamtag186@gmail.com>
2024-07-23 12:33:01 +0400

    wip:middleware update

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -1,21 +1,30 @@
 import jwt from "jsonwebtoken";
 
-const checkToken = (req, res, next) => {
-  try {
-    const token = req.cookies.jwt; // Ensure you have the correct way to access the cookie
-    if (!token) {
-      return res.status(401).json({ error: "Unauthorized: No token provided" });
-    }
+export const checkToken = (req, res, next) => {
+  const token = req.cookies.jwt;
 
+  if (!token) {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id; // Store the user ID in the request object for use in other routes
+    req.user = decoded; // Add decoded token data to the request object
     next();
   } catch (error) {
     if (error.name === "TokenExpiredError") {
-      return res.status(401).json({ error: "Token expired" });
+      const newToken = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res.cookie("jwt", newToken, {
+        maxAge: 604800000, // 7 days
+        httpOnly: true,
+        secure: true,
+        sameSite: "Lax",
+      });
+      req.user = jwt.verify(newToken, process.env.JWT_SECRET);
+      next();
+    } else {
+      return res.status(401).send("Invalid token.");
     }
-    return res.status(500).json({ error: "Failed to authenticate token" });
   }
 };
-
-export default checkToken;
