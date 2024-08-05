7772dd3c88d82d8671f1d16e77d7ba65b3c78891
tamta123 <tamtag186@gmail.com>
2024-08-05 12:44:50 +0400

    update yaml and add logs to checkToken

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -3,11 +3,12 @@ import jwt from "jsonwebtoken";
 export const checkToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split("")[1];
-  console.log("token", token);
 
   if (!token) {
+    console.log("No token provided");
     return res.status(401).send("Access denied. No token provided.");
   }
+  console.log("Received token:", token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // Add decoded token data to the request object
