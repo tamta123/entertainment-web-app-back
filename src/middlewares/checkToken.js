600d6beda90d525586fa60c3b7a13c260e1dbd1b
tamta123 <tamtag186@gmail.com>
2024-08-06 16:20:14 +0400

    wip: correct error

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const checkToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   // Check if Authorization header is present and starts with "Bearer "
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader) {
     console.log("No token provided or wrong header format");
     return res
       .status(401)
